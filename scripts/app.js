eaf8458197fde19434620bfa4fe2a86b36060d69
Brandon Kaufhold <brandonkaufhold@gmail.com>
2018-01-11 21:52:09 -0500

    Added input parsing to get the current url for when ajax is implemented also added a sidebar

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,36 @@
+
 $(document).ready(function () {
-  $('[data-toggle="offcanvas"]').click(function () {
-    $('.row-offcanvas').toggleClass('active')
-  });
+    $('[data-toggle="offcanvas"]').click(function () {
+        $('.row-offcanvas').toggleClass('active')
+    });
+
+    function lowerCaseAllWordsExceptFirstLetters(fontName) {
+        return fontName.replace(/\w\S*/g, function (word) {
+            return word.charAt(0) + word.slice(1).toLowerCase();
+        });
+    }
+
+    function parseFontName(fontName) {
+        // Makes all the characters uppercase
+        fontName = fontName.toUpperCase();
+        // Makes all the characters lowercase except the first letters
+        fontName = lowerCaseAllWordsExceptFirstLetters(fontName);
+        // Delete all the whitespace at the beginning and end of a string
+        fontName = fontName.trim();
+        // Replaces all the remaining spaces with + signs
+        fontUrl = fontName.replace(/ /g, "+");
+        return fontUrl;
+    }
+
+    function getFont(fontUrl) {
+        alert(fontUrl);
+    }
+
+    $("#fontNameInput").keyup(function (ev) {
+        if (ev.keyCode === 13) {
+            var fontName = $("#fontNameInput").val();
+            parseFontName(fontName);
+            getFont(fontUrl);
+        }
+    });
 });
