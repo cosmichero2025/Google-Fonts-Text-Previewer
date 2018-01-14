eceb03d9fb33c088689ec7596c12c7641e875625
Brandon Kaufhold <brandonkaufhold@gmail.com>
2018-01-14 04:35:01 -0500

    1. Added the sidebar 2. Changed some styles 3. Made it so the sidebar shows the top 6 trending web fonts 4. Made the search bar have a random font name show up each time for the example

--- scripts/app.js
+++ scripts/app.js
@@ -1,36 +1,36 @@
-
 $(document).ready(function () {
+    var googleFontAPI = "https://www.googleapis.com/webfonts/v1/webfontskey=AIzaSyCKDRrItgVcxrwi8AZwgqMnK4hyEC5tCoY";
+
+    var googleFontAPI_trending = "https://www.googleapis.com/webfonts/v1/webfonts?sort=trending&key=AIzaSyCKDRrItgVcxrwi8AZwgqMnK4hyEC5tCoY";
+
+    // Controls the sidebar toggle status
     $('[data-toggle="offcanvas"]').click(function () {
-        $('.row-offcanvas').toggleClass('active')
+        $('.row-offcanvas').toggleClass('active');
     });
 
-    function lowerCaseAllWordsExceptFirstLetters(fontName) {
-        return fontName.replace(/\w\S*/g, function (word) {
-            return word.charAt(0) + word.slice(1).toLowerCase();
-        });
-    }
+    // Runs the inital json connection for the sidebar/searchbar placeholder text 
+    $.getJSON(googleFontAPI_trending, function (data, status) {
+        var outputTrendingBox = $('#outputTrendingBox');
 
-    function parseFontName(fontName) {
-        // Makes all the characters uppercase
-        fontName = fontName.toUpperCase();
-        // Makes all the characters lowercase except the first letters
-        fontName = lowerCaseAllWordsExceptFirstLetters(fontName);
-        // Delete all the whitespace at the beginning and end of a string
-        fontName = fontName.trim();
-        // Replaces all the remaining spaces with + signs
-        fontUrl = fontName.replace(/ /g, "+");
-        return fontUrl;
-    }
+        if (status === 'success') {
+            console.log('Connected successfully to the Google Font API');
+            // For the sidebar
+            $.each(data.items, function (i, item) {
+                var content = '<div class="d-flex w-100 justify-content-between list-group-item"><p>' + item.family + '</p><small>' + item.lastModified + '</small></div>';
+                $(outputTrendingBox).append(content);
 
-    function getFont(fontUrl) {
-        alert(fontUrl);
-    }
-
-    $("#fontNameInput").keyup(function (ev) {
-        if (ev.keyCode === 13) {
-            var fontName = $("#fontNameInput").val();
-            parseFontName(fontName);
-            getFont(fontUrl);
+                if (i === 5) {
+                    return false;
+                }
+            });
+            
+            // For the searchbar
+            var fontLength = Math.floor(Math.random() * data.items.length) + 0;
+            var randomFontName = data.items[fontLength].family;
+            $('#searchFontsField').attr('placeholder', 'exp. ' + randomFontName);
+            
+        } else {
+            console.log('Cannot connect to the Google Font API Sadly');
         }
     });
 });
