5263313970602cb53b68e316f9eee9a7ff82c301
Brandon Kaufhold <brandonkaufhold@gmail.com>
2018-01-14 22:13:17 -0500

    Finished the the application it has most of the functionality except maybe in the future might add a searchbar suggestion section

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-# textPreviewer
-A little javascript app that takes the users input for a font then shows a preview of it
+# Text Previewer
 
-test test
+A simple little web app I built because I wasn't that confident in my AJAX/JSON skills so I used this to learn some more. All it really does is take user input from a web font then changes the basic template to that web font's font family. It also displays the top 6 "trending" fonts on the google fonts platform.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Installing
+
+Just ya'know download it and run it in really any web server enviroment. I was using apache but have recently discovered node.js and thats pretty cool.
+
+## Built With
+
+* [Bootstrap](https://getbootstrap.com/) - I'm not good at styling :/
+* [Google Font API](https://developers.google.com/fonts/) - The place that runs my app
+
+## Author
+
+* **Brandon Kaufhold** - *Initial work* - [Cosmichero2025](https://github.com/cosmichero2025)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
-    var googleFontAPI = "https://www.googleapis.com/webfonts/v1/webfontskey=AIzaSyCKDRrItgVcxrwi8AZwgqMnK4hyEC5tCoY";
-
+    var googleFontAPI = "https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyCKDRrItgVcxrwi8AZwgqMnK4hyEC5tCoY";
     var googleFontAPI_trending = "https://www.googleapis.com/webfonts/v1/webfonts?sort=trending&key=AIzaSyCKDRrItgVcxrwi8AZwgqMnK4hyEC5tCoY";
 
     // Controls the sidebar toggle status
@@ -13,24 +12,112 @@ $(document).ready(function () {
         var outputTrendingBox = $('#outputTrendingBox');
 
         if (status === 'success') {
-            console.log('Connected successfully to the Google Font API');
-            // For the sidebar
-            $.each(data.items, function (i, item) {
-                var content = '<div class="d-flex w-100 justify-content-between list-group-item"><p>' + item.family + '</p><small>' + item.lastModified + '</small></div>';
-                $(outputTrendingBox).append(content);
+            console.log('Connected successfully to the Google Font API: Inital Data');
 
+            $.each(data.items, function (i, item) {
+                var content = '<div class="trending-font d-flex w-100 justify-content-between list-group-item" ><p>' + item.family + '</p><small>' + item.lastModified + '</small></div>';
+                $(outputTrendingBox).append(content);
+                
                 if (i === 5) {
                     return false;
                 }
             });
-            
-            // For the searchbar
+
+            // For the searchbar's random font name placeholder
             var fontLength = Math.floor(Math.random() * data.items.length) + 0;
             var randomFontName = data.items[fontLength].family;
             $('#searchFontsField').attr('placeholder', 'exp. ' + randomFontName);
-            
         } else {
-            console.log('Cannot connect to the Google Font API Sadly');
+            console.log('Couldnt to server for searchFonts: Inital Data');
         }
+    });
+    /* End pre json code */
+
+    // Parses font names to be used in two different formats
+    function createUrl(fontName) {
+        var apiUrl = [];
+        // Take initial Link
+        apiUrl.push('https://fonts.googleapis.com/css?family=');
+
+        // Removes all whitespace in the beginning or end of the string
+        fontName = fontName.trim();
+
+        // Makes all the letter lowercase
+        fontName = fontName.toLowerCase();
+
+        // Makes all the first letters uppercase
+        fontName = fontName.replace(/\w\S*/g, function (txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        });
+
+        // Addes the name and replaces the spaces with +
+        apiUrl.push(fontName.replace(/ /g, '+'));
+
+        // Takes the two haves of the string array and puts them together
+        var fontUrl = apiUrl.join('');
+
+        return {
+            fontName: fontName,
+            fontUrl: fontUrl
+        };
+    }
+
+    var $searchFontsField = $('#searchFontsField');
+    var $searchFontsButton = $('#searchFontsButton');
+
+    // When the user selects one of the trending fonts
+    $(document).on('click', '.trending-font', function () {
+        var $sideBarFont = $(this).find('p').text();
+        $searchFontsField.val($sideBarFont);
+        $searchFontsButton.click();
+    });
+
+    // When the user presses enter instead of using the button
+    $searchFontsField.keypress(function (e) {
+        var key = e.which;
+        if (key == 13) {
+            $searchFontsButton.click();
+            $searchFontsField.val('');
+            return false;
+        }
+    });
+
+    // All the logic for when the user wants a new font shown
+    $searchFontsButton.click(function () {
+        var fontName = $searchFontsField.val();
+        // Create the url from the fontName
+        var fonts = createUrl(fontName);
+        var fontFamily = fonts.fontName;
+        var fontCSS = fonts.fontUrl;
+
+        var fontAddress = 'https://fonts.google.com/specimen/' + fonts.fontName.replace(/ /g, '+');
+
+        var $fontHeader = $('#fontName');
+        var $fontProfile = $('#fontProfile');
+        $searchFontsField.val('');
+        $.getJSON(googleFontAPI, function (data, status) {
+            if (status === 'success') {
+                console.log('Connected successfully to the Google Font API: Search Bar');
+
+                // Adds the font stylesheet to the page head
+                $("head link[rel='stylesheet']").last().after('<link rel="stylesheet" href="' + fontCSS + '">');
+
+                // Changes the header text to the font family
+                $('#fontOutput').css("font-family", fontFamily);
+                $fontHeader.text(fontFamily);
+
+                // Changes the link for the font profile link
+                $fontProfile.attr('href', fontAddress);
+                $fontProfile.text(fontAddress);
+
+                // Reinstances the random font name in the search field
+                fontLength = Math.floor(Math.random() * data.items.length) + 0;
+                randomFontName = data.items[fontLength].family;
+                $('#searchFontsField').attr('placeholder', 'exp. ' + randomFontName);
+
+            } else {
+                console.log('Couldnt to server for searchFonts: Search Bar');
+            }
+        });
     });
 });
